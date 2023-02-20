2b7ccc925555e7df4090e4974573ece5e73cbc9d
regular-meet-8 <eddiekim444@gmail.com>
2023-02-20 09:42:49 +0300

    triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  class TriangleError < StandardError; end
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort
+    validate_triangle
+  end
+
+  def kind
+    if @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if @sides.any? { |side| side <= 0 } || @sides[0] + @sides[1] <= @sides[2]
+      raise TriangleError, "Invalid triangle"
+    end
+  end
 end
